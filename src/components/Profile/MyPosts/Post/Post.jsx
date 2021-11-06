f70a57f4a8a1227a0546076201dae5a9dff1cc0e
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-11-06 09:12:14 +0200

    3

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,12 @@
+import s from "./Post.module.css";
+
+const Post = () => {
+  return (
+    <div className={s.item}>
+      <img src="https://klike.net/uploads/posts/2019-03/1551511801_1.jpg" alt='avatar' />
+      Post 1
+    </div>
+  );
+};
+
+export default Post;
