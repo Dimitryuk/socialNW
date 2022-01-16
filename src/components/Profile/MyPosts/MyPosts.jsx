18ad99ec12a877c8651a8ea2e32244b9fb264731
Dimitryuk <79190561+Dimitryuk@users.noreply.github.com>
2022-01-16 12:45:06 +0200

    39 lesson done

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,46 +1,50 @@
-import s from './MyPosts.module.css'
-import Post from './Post/Post'
+import s from './MyPosts.module.css';
+import Post from './Post/Post';
 import React from 'react';
+import {
+  updateNewPostTextActionCreator,
+  addPostActionCreator,
+} from '../../redux/state';
 
+const MyPosts = props => {
+  //   const posts =[
+  //     {message: 'Hi.how are you?', id:1, likesCount :18},
+  //     {message: 'Its my first post', id:2, likesCount : 15},
 
-const MyPosts = (props) => {
-//   const posts =[
-//     {message: 'Hi.how are you?', id:1, likesCount :18},
-//     {message: 'Its my first post', id:2, likesCount : 15},
-   
-// ]
+  // ]
 
-let postsElements = props.posts.map((p)=><Post message = {p.message} likesCount={p.likesCount}/>)
-console.log(props);
+  let postsElements = props.posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
+  console.log(props);
 
-let newPostElement= React.createRef()
+  let newPostElement = React.createRef();
 
-let addPost=()=>{
-  
-  props.dispatch({type:'ADD-POST'})
-}
-let onPostChange =()=>{
-  let text= newPostElement.current.value
-  props.dispatch({type:'UPDATE-NEW-POST-TEXT', newText:text})
-  // console.log(props);
-}
+  let addPost = () => {
+    props.dispatch(addPostActionCreator());
+  };
+  let onPostChange = () => {
+    let text = newPostElement.current.value;
+    props.dispatch(updateNewPostTextActionCreator(text));
+    // console.log(props);
+  };
   return (
-
     <div className={s.postsBlock}>
-     <h3>My Posts</h3>
-      
+      <h3>My Posts</h3>
+
       <div>
         <div>
-          <textarea ref={newPostElement} onChange={onPostChange} value={props.newPostText} />
+          <textarea
+            ref={newPostElement}
+            onChange={onPostChange}
+            value={props.newPostText}
+          />
         </div>
         <div>
-           <button onClick={addPost}>Add post</button>
-         </div>
-        
-       </div>
-      <div className={s.posts}>
-        {postsElements}
+          <button onClick={addPost}>Add post</button>
+        </div>
       </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,75 +1,69 @@
-let store={
-  _state:{
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+let store = {
+  _state: {
     profilePage: {
-       posts:[
-    {message: 'Hi.how are you?', id:1, likesCount :18},
+      posts: [
+        { message: 'Hi.how are you?', id: 1, likesCount: 18 },
         { message: 'Its my first post', id: 2, likesCount: 15 },
-    {message: 'Its my second post', id:3, likesCount : 3},
-   
+        { message: 'Its my second post', id: 3, likesCount: 3 },
       ],
-      newPostText:"it-kamasutra"
-         
+      newPostText: 'it-kamasutra',
     },
     dialogsPage: {
-         messages:[
-    {message: 'Hello', id:1},
-    {message: 'How are you', id:2},
-    {message: 'Im fine', id:3},
-    {message: 'Yo', id:4},
-    {message: 'Yo', id:5},
+      messages: [
+        { message: 'Hello', id: 1 },
+        { message: 'How are you', id: 2 },
+        { message: 'Im fine', id: 3 },
+        { message: 'Yo', id: 4 },
+        { message: 'Yo', id: 5 },
       ],
-       dialogs:[
-    {name: 'Dima', id:1},
-    {name: 'Anfrey', id:2},
-    {name: 'Sveta', id:3},
-    {name: 'Sasha', id:4},
-    {name: 'Viktor', id:5},
-    {name: 'Valera', id:6},
-    
-  ]
-    }
-  
+      dialogs: [
+        { name: 'Dima', id: 1 },
+        { name: 'Anfrey', id: 2 },
+        { name: 'Sveta', id: 3 },
+        { name: 'Sasha', id: 4 },
+        { name: 'Viktor', id: 5 },
+        { name: 'Valera', id: 6 },
+      ],
+    },
   },
-  getState(){
-   
-    return this._state
+  getState() {
+    return this._state;
   },
-  _callSubscriber (){
-    console.log('state changed')
+  _callSubscriber() {
+    console.log('state changed');
   },
-  
-  subscribe (observer){
-    this._callSubscriber=observer
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
-  
-  dispatch(action){
-    
-    if (action.type === "ADD-POST"){
+
+  dispatch(action) {
+    if (action.type === ADD_POST) {
       let newPost = {
-        id:4,
+        id: 4,
         message: this._state.profilePage.newPostText,
-        likesCount: 0
+        likesCount: 0,
       };
-      this._state.profilePage.posts.push(newPost)
-      this._state.profilePage.newPostText=''
-      this._callSubscriber(this._state)
-    
-    } else if(action.type === "UPDATE-NEW-POST-TEXT"){
-      this._state.profilePage.newPostText=action.newText
-      this._callSubscriber(this._state)
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
     }
+  },
+};
 
-  }
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
-}
+export const updateNewPostTextActionCreator = text => {
+  return {
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
+  };
+};
 
-
-export default store
-window.store=store
-
-
-
-
-
-
-
+export default store;
+window.store = store;
