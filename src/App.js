68fec52fbc34bb8fbfe1f928a4e687b3cf23970c
Dimitryuk <79190561+Dimitryuk@users.noreply.github.com>
2022-01-16 11:06:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
 // import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
-import Navbar from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs";
-import {Route,  BrowserRouter, Routes } from 'react-router-dom'
+import './App.css';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
-import { addPost } from "./components/redux/state";
-
-
+import { addPost } from './components/redux/state';
 
 function App(props) {
-  console.log(props)
+  console.log(props);
 
-  let DialogComponent = () => <Dialogs state={ props.state.dialogsPage}/>
-let ProfileComponent = ()=><Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>
+  let DialogComponent = () => <Dialogs state={props.state.dialogsPage} />;
+  let ProfileComponent = () => (
+    <Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />
+  );
   return (
     <BrowserRouter>
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
 
-      <div className='app-wrapper-content'>
-         <Routes>
-       
-         <Route path='/dialogs' element={<DialogComponent/>}/>
-        < Route path='/profile' element={<ProfileComponent/>}/>
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" element={<DialogComponent />} />
+            <Route path="/profile" element={<ProfileComponent />} />
 
-         {/* <Route exact path='/dialogs' element ={<DialogComponent/>}/>
+            {/* <Route exact path='/dialogs' element ={<DialogComponent/>}/>
 
         <Route path='/profile' element ={<ProfileComponent />} /> */}
-          {/* <Route exact path='/dialogs' element ={<DialogComponent/>}/> */}
-         
-          
+            {/* <Route exact path='/dialogs' element ={<DialogComponent/>}/> */}
+          </Routes>
 
-        </Routes> 
-        
-{/* <Profile/>
+          {/* <Profile/>
 <Dialogs/> */}
-     
+        </div>
+
+        <footer />
       </div>
-      
-      
-      <footer />
-    </div>
     </BrowserRouter>
   );
 }
