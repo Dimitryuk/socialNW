144a1dd365ebc4b467d42d8d4506662bf9056588
Alexander Dimitryuk <dimitryuk38@gmail.com>
2022-01-31 13:51:36 +0200

    57 lesson done

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -7,21 +7,26 @@ import {
   unfollowActionCreator,
   setCurrentPageActionCreator,
   setUsersTotalCountActionCreator,
+  toggleIsFetchingActionCreator,
 } from '../redux/users-reducer';
 import Users from './Users';
+import Preloader from '../common/Preloader/Preloader';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
+    this.props.toggleIsFetching(true)
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
       )
       .then(response => {
+        this.props.toggleIsFetching(false)
         this.props.setUsers(response.data.items);
         this.props.setTotalUsersCount(response.data.totalCount);
       });
   }
   onPageChanged = pageNumber => {
+       this.props.toggleIsFetching(true)
     this.props.setCurrentPage(pageNumber);
     axios
       .get(
@@ -29,10 +34,14 @@ class UsersContainer extends React.Component {
       )
       .then(response => {
         this.props.setUsers(response.data.items);
+        this.props.toggleIsFetching(false)
       });
   };
   render() {
     return (
+      <>
+        {this.props.isFetching ? <Preloader/>
+        : null}
       <Users
         totalUsersCount={this.props.totalUsersCount}
         pageSize={this.props.pageSize}
@@ -41,7 +50,9 @@ class UsersContainer extends React.Component {
         users={this.props.users}
         follow={this.props.follow}
         unfollow={this.props.unfollow}
-      />
+
+        />
+        </>
     );
   }
 }
@@ -52,6 +63,7 @@ let mapStateToProps = state => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
+    isFetching:state.usersPage.isFetching,
   };
 };
 
@@ -73,6 +85,9 @@ let mapDispatchToProps = dispatch => {
     setTotalUsersCount: totalCount => {
       dispatch(setUsersTotalCountActionCreator(totalCount));
     },
+    toggleIsFetching: (isFetching) => {
+      dispatch(toggleIsFetchingActionCreator(isFetching))
+    }
   };
 };
 
